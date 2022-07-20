5839f5014a072b45d75f48bd1306c0cb80ec3267
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2022-07-20 08:50:53 +0100

    new

--- src/components/Trial.jsx
+++ src/components/Trial.jsx
@@ -8,13 +8,13 @@ const Trial = () => {
           <div className="container p-sm-5 p-4">
             <div className="row g-5">
               <div className="col-lg-7 col-md-10 mx-auto text-lg-start text-center">
-                 <h6 style={{fontSize: "13px", lineHeight: "20px"}} className="fw-bold">FREE TRIAL</h6>
+                 <h6 style={{fontSize: "13px", lineHeight: "20px", color: "#be9cfd"}} className="fw-bold">FREE TRIAL</h6>
                  <div>
                     <h2 className="fw-bold mb-0 sub-title">Start your 1-month free trial</h2>
-                    <p style={{lineHeight: "24px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p style={{lineHeight: "24px", color: "rgb(129, 147, 178)"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                  </div>
 
-                 <div className='mb-4'>
+                 <div className='mb-2'>
                     <div className="trial-email d-sm-flex align-items-center" style={{width: "100%"}}>
                         <input className='trial-email-input' type="email" name="" placeholder='Your email' id="" />
                         <button className='trial-email-btn'>Submit</button>
@@ -22,7 +22,7 @@ const Trial = () => {
                  </div>
 
                  <div>
-                    <div className="d-sm-flex gap-lg-5 gap-sm-3 justify-content-lg-start justify-content-center">
+                    <div style={{fontSize: "13px"}} className="d-sm-flex gap-lg-5 gap-sm-3 justify-content-lg-start justify-content-center">
                       <p className='mb-0'><span>+</span> Cancel anytime</p>
                       <p><span>+</span> No credit card required</p>
                     </div>
