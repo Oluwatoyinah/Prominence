5ffa93f580e1d47d2e1bcc00f2e286299897282f
Oluwatoyinah <akinbobolaoluwatoyin25@gmail.com>
2022-07-20 09:30:49 +0100

    trial email fiixed

--- src/components/Trial.jsx
+++ src/components/Trial.jsx
@@ -9,20 +9,20 @@ const Trial = () => {
             <div className="row g-5">
               <div className="col-lg-7 col-md-10 mx-auto text-lg-start text-center">
                  <h6 style={{fontSize: "13px", lineHeight: "20px", color: "#be9cfd"}} className="fw-bold">FREE TRIAL</h6>
-                 <div>
+                 <div className='mb-2'>
                     <h2 className="fw-bold mb-0 sub-title">Start your 1-month free trial</h2>
-                    <p style={{lineHeight: "24px", color: "rgb(129, 147, 178)"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    {/* <p style={{lineHeight: "24px", color: "rgb(129, 147, 178)"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> */}
                  </div>
 
                  <div className='mb-2'>
-                    <div className="trial-email d-sm-flex align-items-center" style={{width: "100%"}}>
-                        <input className='trial-email-input' type="email" name="" placeholder='Your email' id="" />
-                        <button className='trial-email-btn'>Submit</button>
+                    <div className="trial-email d-flex align-items-center" style={{width: "100%"}}>
+                        <input className='trial-email-input flex-grow-1' type="email" name="" placeholder='Your email' id="" />
+                        <button className='trial-email-btn inline-flex'>Submit</button>
                     </div> 
                  </div>
 
                  <div>
-                    <div style={{fontSize: "13px"}} className="d-sm-flex gap-lg-5 gap-sm-3 justify-content-lg-start justify-content-center">
+                    <div style={{fontSize: "13px",color: "rgb(129, 147, 178)"}} className="d-sm-flex gap-lg-5 gap-sm-3 justify-content-lg-start justify-content-center">
                       <p className='mb-0'><span>+</span> Cancel anytime</p>
                       <p><span>+</span> No credit card required</p>
                     </div>
@@ -31,7 +31,7 @@ const Trial = () => {
 
               <div className="col-lg-5 d-none d-lg-block">
                 <div>
-                  <img src="" alt="trail illustration will be here" />
+                  <img src="" alt="trial illustration will be here" />
                 </div>
               </div>
             </div>
